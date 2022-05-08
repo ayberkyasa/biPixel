8478ce515aaa890b61833a5b7f22036e189808ef
Doğa Ersoy <63476993+dogaersoy@users.noreply.github.com>
2022-05-09 00:47:57 +0300

    Backend Search Movie was added

--- src/backend/src/main/java/src/backend/controller/RentController.java
+++ src/backend/src/main/java/src/backend/controller/RentController.java
@@ -20,10 +20,23 @@ public class RentController {
     public List<HashMap<String, Object>> searchBy(@RequestParam("key") String searchKey,
                                                   @RequestParam("type") String searchType,
                                                   @RequestParam("userId") Integer userId) {
-        // TODO: search the movies by title, director, production year, actor
-        // WARNING: Filtering will be done in the frontend.
-        // TODO: If the user specified by "userId" already rented the movie, do not show it on the returned list.
-        return null;
+        
+        try {
+            List<HashMap<String, Object>> returned = connector.executeQuery("SELECT DISTINCT title, director_first_name, director_middle_name, director_last_name, overall_rating, production_year, price" +
+                    " FROM (movie NATURAL JOIN act NATURAL JOIN actor NATURAL JOIN direct NATURAL JOIN director NATURAL JOIN movie_genre NATURAL JOIN genre) LEFT JOIN rent_movie ON rent_movie.movie_id = movie.movie_id" +
+                    " WHERE movie.movie_id NOT IN (SELECT movie_id FROM rent_movie WHERE user_id = " + userId +")" +
+                    " AND (title RLIKE '" + searchKey + "' OR actor_first_name RLIKE '" + searchKey + "' OR actor_middle_name RLIKE '" + searchKey + "' OR actor_last_name RLIKE '" + searchKey + "'" +
+                    " OR director_first_name RLIKE '" + searchKey + "' OR director_middle_name RLIKE '" + searchKey + "' OR director_last_name RLIKE '" + searchKey + "')");
+
+            if (returned.size() == 0) {
+                HashMap<String, Object> result = new HashMap<>();
+                result.put("result", "No movie found.");
+                returned.add(result);
+            }
+            return returned;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @PostMapping("/rent")

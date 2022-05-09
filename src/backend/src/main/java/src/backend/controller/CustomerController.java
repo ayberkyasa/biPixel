98fa5725b0fd1605d7f665de821ac4a18a08c579
Doğa Ersoy <63476993+dogaersoy@users.noreply.github.com>
2022-05-09 22:33:52 +0300

    requestMovie added

--- src/backend/src/main/java/src/backend/controller/CustomerController.java
+++ src/backend/src/main/java/src/backend/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package src.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,35 @@ public class CustomerController {
     }
 
     @PostMapping("/request-movie")
-    public HashMap<String, Object> requestMovie(@RequestBody HashMap<String, Object> requestBody) {
+    public ResponseEntity<HashMap<String, Object>> requestMovie(@RequestBody HashMap<String, Object> requestBody) {
         // TODO: From RequestBody, all information about movie will come. Check ER Diagram from requested_movie table.
-        return null;
+        HashMap<String, Object> result = new HashMap<>();
+        List<HashMap<String, Object>> movieList = connector.executeQuery("SELECT * FROM movie NATURAL JOIN direct NATURAL JOIN director " +
+                "WHERE title = '" + requestBody.get("title") + "' AND production_year = " + requestBody.get("production_year") + " AND " +
+                "director_full_name = '" + requestBody.get("directors_full_name") + "'");
+
+        if (movieList.size() != 0) {
+            result.put("result", "Movie already exists.");
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+        else {
+            List<HashMap<String, Object>> returned = connector.executeQuery("SELECT * FROM requested_movie NATURAL JOIN request" +
+                    " WHERE user_id = " + requestBody.get("userId") + " AND title = '" + requestBody.get("title") + "' AND production_year = " + requestBody.get("production_year") + " " +
+                    " AND directors_full_name = '" + requestBody.get("directors_full_name") + "'");
+
+            if (returned.size() != 0) {
+                result.put("result", "You have already made this request.");
+                return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+            }
+
+            connector.executeUpdate("INSERT INTO requested_movie(title, directors_full_name, actors_first_name, production_year, duration, genres)" +
+                    " VALUES('" + requestBody.get("title") + "', '" + requestBody.get("directors_full_name") + "', '" + requestBody.get("actors_full_name") + "', " + requestBody.get("production_year") + "" +
+                    ", " + requestBody.get("duration") + ", '" + requestBody.get("genres") + "')");
+            connector.executeUpdate("INSERT INTO request(user_id, req_movie_id)" +
+                    " VALUES(" + requestBody.get("userId") + ", (SELECT MAX(req_movie_id) FROM requested_movie))");
+
+            result.put("result", "Your request is sent.");
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
     }
 }

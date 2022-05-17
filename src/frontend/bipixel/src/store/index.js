44d90c4683d1e29943a64a8d568ea09f5d3e9ea0
Ayberk Yaşa <63598688+ayberkyasa@users.noreply.github.com>
2022-05-17 21:34:13 +0300

    Update index.js

--- src/frontend/bipixel/src/store/index.js
+++ src/frontend/bipixel/src/store/index.js
@@ -5,17 +5,19 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isLoggedIn: false,
+    isLoggedIn: false || (localStorage.getItem("isLoggedIn") === "true"),
     uid: null,
     userType: "",
   },
   mutations: {
     logIn(state, payload) {
+      localStorage.setItem("isLoggedIn", true);
       state.isLoggedIn = true;
       state.uid = payload.id;
       state.userType = payload.userType;
     },
     logOut(state) {
+      localStorage.removeItem("isLoggedIn");
       state.isLoggedIn = false;
       state.uid = null;
       state.userType = "";
